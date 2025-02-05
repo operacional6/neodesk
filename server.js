a056680b4c21ce0a2830e5ad534396df88f34c29
Gabriele Almeida <naogabe@gmail.com>
2025-02-05 10:32:56 -0300

    fix: add import from sendToAsana function

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const app = express();
 require('dotenv').config();
+const sendToAsana = require("./asana"); 
 
 app.use(express.static(path.join(__dirname, "public")));
 app.use('/node_modules', express.static(path.join(__dirname, 'node_modules')));
